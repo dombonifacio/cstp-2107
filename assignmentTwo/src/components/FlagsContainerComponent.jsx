2acdd9f6cc303cf971aec2a95345386eb11c85cb
dombonifacio <nikkibonifacio184@gmail.com>
2023-06-13 19:23:23 -0700

    add

--- assignmentTwo/src/components/FlagsContainerComponent.jsx
+++ assignmentTwo/src/components/FlagsContainerComponent.jsx
@@ -4,17 +4,24 @@ import { useState, useEffect} from "react"
 
 export const FlagsContainerComponent = () => {
 
+    const [flags, setFlags] = useState([])
+    
+
 
     axios
-    .get("https://restcountries.com/v3.1/all")
-        .then((res) => {
-        console.log(res.data)
+        .get("https://restcountries.com/v3.1/all")
+        .then((res) => 
+        {
+        setFlags(res.data)
     })
 
 
+    console.log(flags)
+    
     
 
     return (
+        
         <div>
 
         </div>
