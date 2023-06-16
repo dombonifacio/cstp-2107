7d7b0bcafaf0b0fb4f310c148e54e098e6999de8
Dominique Bonifacio <nikkibonifacio184@gmail.com>
2023-06-16 16:30:58 -0700

    add

---
+++ exercises/githubAPI/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <input type="text" className='text-slate-500 p-3 rounded-lg text-3xl' placeholder='Enter Github Username'/>
+      </div>
+    </>
+  )
+}
+
+export default App
